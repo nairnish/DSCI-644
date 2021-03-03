549756f622bde57f77e1abd1d1473b8efd110f41
prajwalkrishna2@gmail.com <prajwalkrishna2@gmail.com>
2021-03-03 18:57:26 -0500

    Linear SVC - the better model so far

--- Prajwal/attempt_1.py
+++ Prajwal/attempt_1.py
@@ -1,5 +1,4 @@
 import warnings
-
 import numpy as np
 import pandas as pd
 from sklearn.compose import ColumnTransformer
@@ -9,7 +8,6 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfVectorizer
 from collections import OrderedDict
 from sklearn.tree import DecisionTreeClassifier
-
 from sklearn.model_selection import StratifiedKFold, GridSearchCV
 from sklearn.naive_bayes import GaussianNB, BernoulliNB
 from sklearn.naive_bayes import MultinomialNB
@@ -25,19 +23,21 @@ import nltk
 class my_model():
 
     def fit(self, X, y):
-        # do not exceed 29 mins
         X = self.clean_all_data(X)
         
         # self.preprocessor = TfidfVectorizer(stop_words='english', norm='l2',
         #                                     use_idf=True, smooth_idf=True, ngram_range=(1,5))
         # XX = self.preprocessor.fit_transform(X["combined_text"])
-
         # required_cat_features = ['state']
+
         required_text_features = ['combined_text']
+
         # required_binary_features = ['has_company_logo']
         # binary_transformer = Pipeline(steps=[('label', OneHotEncoder(handle_unknown='ignore'))])
         # cat_transformer = Pipeline(steps=[('onehot', OneHotEncoder(handle_unknown='ignore'))])
+
         text_transformer = Pipeline(steps=[('tfidf', TfidfVectorizer(ngram_range=(1, 2)))])
+
         # text_transformer = Pipeline(steps=[('nltk', nltk.tokenize)])
         # text_transformer = Pipeline(steps=[('tfidf', TfidfVectorizer(stop_words='english', norm='l2', use_idf=False, smooth_idf=False))])
 
@@ -51,10 +51,13 @@ class my_model():
         )
 
         # log_reg_pipe = Pipeline(steps=[('preprocessor', preprocessor),
-        #                                ('classifier', SGDClassifier(class_weight="balanced"))])
+        #                                ('classifier', MLPClassifier())])
 
+        # log_reg_pipe = Pipeline(steps=[('preprocessor', preprocessor),
+        #                                ('classifier', SGDClassifier(class_weight="balanced"))])
+        #
         log_reg_pipe = Pipeline(steps=[('preprocessor', preprocessor),
-                                       ('classifier', LinearSVC())])
+                                       ('classifier', LinearSVC(random_state=0, tol=1e-5))])
 
         self.clf = log_reg_pipe
 
@@ -65,17 +68,22 @@ class my_model():
     def predict(self, X):
         # remember to apply the same preprocessing in fit() on test data before making predictions
         X = self.clean_all_data(X)
+
         # drop_cols = ['telecommuting', 'has_questions', 'country', 'city']
         # X = X.drop(drop_cols, axis=1)
         # bin_features = ['telecommuting', 'has_company_logo', 'has_questions']
         # text_features = ['title', 'description', 'requirements']
         # cat_features = ['country', 'state', 'city']
         # required_cat_features = ['state']
+
         required_text_features = ['combined_text']
+
         # required_binary_features = ['has_company_logo']
         # binary_transformer = Pipeline(steps=[('label', OneHotEncoder(handle_unknown='ignore'))])
         # cat_transformer = Pipeline(steps=[('onehot', OneHotEncoder(handle_unknown='ignore'))])
+
         text_transformer = Pipeline(steps=[('tfidf', TfidfVectorizer(ngram_range=(1, 2)))])
+
         # text_transformer = Pipeline(steps=[('nltk', nltk.tokenize)])
         # text_transformer = Pipeline(steps=[('tfidf', TfidfVectorizer(stop_words='english', norm='l2', use_idf=False, smooth_idf=False))])
 
@@ -87,9 +95,11 @@ class my_model():
                   for feature_name in required_text_features]
             ]
         )
+
         # log_reg_pipe = Pipeline(steps=[('preprocessor', preprocessor),
         #                                ('classifier', LogisticRegression())])
         # self.clf = log_reg_pipe
+
         predictions = self.clf.predict(X)
         return predictions
 
