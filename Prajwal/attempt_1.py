33500c971517753405fde093d82a5d78234922c7
prajwalkrishna2@gmail.com <prajwalkrishna2@gmail.com>
2021-04-06 23:02:47 -0400

    new changes added

--- Prajwal/attempt_1.py
+++ Prajwal/attempt_1.py
@@ -11,6 +11,7 @@ from sklearn.pipeline import Pipeline
 import re
 from nltk import sent_tokenize, word_tokenize
 nltk.download('punkt')
+nltk.download('wordnet')
 from nltk.stem.snowball import SnowballStemmer,PorterStemmer
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import stopwords
@@ -33,27 +34,13 @@ class my_model():
     def fit(self, X, y):
         # do not exceed 29 mins
         X = self.clean_all_data(X)
-        
-        # self.preprocessor = TfidfVectorizer(stop_words='english', norm='l2',
-        #                                     use_idf=True, smooth_idf=True, ngram_range=(1,5))
-        # XX = self.preprocessor.fit_transform(X["combined_text"])
-        # required_cat_features = ['state']
 
-        required_text_features = ['combined_text']
-
-        # required_binary_features = ['has_company_logo']
-        # binary_transformer = Pipeline(steps=[('label', OneHotEncoder(handle_unknown='ignore'))])
-        # cat_transformer = Pipeline(steps=[('onehot', OneHotEncoder(handle_unknown='ignore'))])
-
+        #required_text_features = ['combined_text']
+        required_text_features = ['combined_text', 'tokens', 'stems', 'lemma']
         text_transformer = Pipeline(steps=[('tfidf', TfidfVectorizer(ngram_range=(1, 2)))])
-
-        # text_transformer = Pipeline(steps=[('nltk', nltk.tokenize)])
-        # text_transformer = Pipeline(steps=[('tfidf', TfidfVectorizer(stop_words='english', norm='l2', use_idf=False, smooth_idf=False))])
 
         preprocessor = ColumnTransformer(
             transformers=[
-                # ('bin', binary_transformer, required_binary_features),
-                # ('cat', cat_transformer, required_cat_features),
                 *[(feature_name, text_transformer, feature_name)
                   for feature_name in required_text_features]
             ]
@@ -69,23 +56,16 @@ class my_model():
 
         self.clf.fit(X, y)
 
-        # print(XX)
-
     def predict(self, X):
         # remember to apply the same preprocessing in fit() on test data before making predictions
         X = self.clean_all_data(X)
 
-        required_text_features = ['combined_text']
+        required_text_features = ['combined_text','tokens','stems','lemma']
 
         text_transformer = Pipeline(steps=[('tfidf', TfidfVectorizer(ngram_range=(1, 2)))])
 
-        # text_transformer = Pipeline(steps=[('nltk', nltk.tokenize)])
-        # text_transformer = Pipeline(steps=[('tfidf', TfidfVectorizer(stop_words='english', norm='l2', use_idf=False, smooth_idf=False))])
-
         preprocessor = ColumnTransformer(
             transformers=[
-                # ('bin', binary_transformer, required_binary_features),
-                # ('cat', cat_transformer, required_cat_features),
                 *[(feature_name, text_transformer, feature_name)
                   for feature_name in required_text_features]
             ]
@@ -134,13 +114,12 @@ class my_model():
         for columns in text_columns:
             self.remove_stopwords_from_data_train(df,columns)
         
-        # Add lemmatization
-        
         # Add tokenization
         # df = nltk.word_tokenize(df['combined_text'])
         df['combined_text'] = df['combined_text'].str.lower()
         df['tokens'] = df.apply(self.tokenization_df, axis = 1)
         df['stems'] = df.apply(self.stem_df,axis = 1)
+        df['lemma'] = df.apply(self.lemma_df, axis=1)
         return df
 
     # def tokenization_df(self,data_frame):
@@ -153,7 +132,7 @@ class my_model():
 
     def tokenization_df(self,data_frame):
         data_frame_token = data_frame['combined_text']
-        tokens = nltk.sent_tokenize(data_frame_token)
+        tokens = nltk.word_tokenize(data_frame_token)
 
         token_words = [words for words in tokens if words.isalpha()]
         return token_words
@@ -163,6 +142,12 @@ class my_model():
         data_frame_token = data_frame['combined_text']
         stemmed = [stemming.stem(word) for word in data_frame_token]
         return stemmed
+
+    def lemma_df(self,data_frame):
+        lemming = WordNetLemmatizer()
+        data_frame_lemma = data_frame['combined_text']
+        lemma = [lemming.lemmatize(word) for word in data_frame_lemma]
+        return lemma
     
     def remove_stopwords_from_data_train(self,data_frame, column_name):
         data_frame[column_name] = data_frame[column_name].apply(lambda x: " ".join([i for i in x.lower().split() if i not in self.all_genism_stop_words]))
